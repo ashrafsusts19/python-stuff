270450900dafe8eeb81ae26a0c8340c961f56f9a
ashrafsusts19 <66487468+ashrafsusts19@users.noreply.github.com>
2021-01-12 18:26:43 +0600

    Update Minesweeper.py

--- Tkinter Apps/Minesweeper.py
+++ Tkinter Apps/Minesweeper.py
@@ -1,16 +1,9 @@
-"""
-stack = list of index to check
-closed = list of buttons disabled
-examine(index) = examines the surrounding of map[index]
-    examine doesn't pop
-"""
-
 from tkinter import *
 import random
 
 def click(index):
-    global mfield, map, fr_dbar, stack, closed, row, col, bomb, isgameover
-    if index in closed or isgameover:
+    global mfield, map, fr_dbar, stack, closed, row, col, bomb, isgameover, flag_map, flags, lab_flag
+    if index in closed or isgameover or index in flag_map:
         return
     if map[index] == "b":
         lab_over = Label(fr_dbar, text = "Game Over! You lost")
@@ -30,6 +23,12 @@ def click(index):
             mfield[cl_ind].config(bg = "grey")
         else:
             mfield[cl_ind].config(text = str(cl_exam))
+    for i in flag_map:
+        if i in closed:
+            mfield[i].config(text="   ", fg="black")
+            flags += 1
+            flag_map.remove(i)
+    lab_flag.config(text="Flags: " + str(flags))
     if len(closed) == row*col-bomb:
         lab_over = Label(fr_dbar, text="You Won!")
         lab_over.pack()
@@ -60,13 +59,15 @@ def examine(index):
 
 
 def first_click(index):
-    global map, row, col, bomb, mfield
+    global map, row, col, bomb, mfield, lab_flag, flags
     map = ["b" for i in range(bomb)] + ["" for i in range(row * col - bomb)]
     random.shuffle(map)
     while map[index] == "b":
         random.shuffle(map)
     for i in range(row * col):
         mfield[i].config(command=lambda x=i: click(x))
+        mfield[i].bind('<Button-3>', lambda eff, x=i: rightclick(event=eff, index=x))
+    lab_flag.config(text="Flags: "+str(flags))
     click(index)
 
 
@@ -75,15 +76,34 @@ def game_over():
     isgameover = True
     for i in range(row*col):
         if map[i] == "b":
-            mfield[i].config(text="B")
+            mfield[i].config(text="B", fg= "red")
+
+
+def rightclick(event, index):
+    global flag_map, closed, mfield, flags, lab_flag
+    if index in closed or isgameover:
+        return
+    if index in flag_map:
+        mfield[index].config(text="   ", fg = "black")
+        flags += 1
+        flag_map.remove(index)
+    elif flags>0:
+        mfield[index].config(text=" ! ", fg="blue")
+        flags -= 1
+        flag_map.append(index)
+    lab_flag.config(text="Flags: "+str(flags))
 
 
 isgameover = False
 row = 10
 col = 10
 bomb = 10
+flags = bomb
 
 root = Tk()
+
+fr_tbar = LabelFrame(root)
+fr_tbar.grid(row=0, column=0)
 
 fr_game = LabelFrame(root)
 fr_game.grid(row=1, column=0)
@@ -91,12 +111,12 @@ fr_game.grid(row=1, column=0)
 fr_dbar = LabelFrame(root)
 fr_dbar.grid(row=2, column=0)
 
+lab_flag = Label(fr_tbar, text = "Flags: --")
+lab_flag.grid(row=0, column=0)
 
 stack = []
 closed = []
-
-map = ["b" for i in range(bomb)] + ["" for i in range(row*col-bomb)]
-random.shuffle(map)
+flag_map = []
 
 mfield = []
 for i in range(row*col):
